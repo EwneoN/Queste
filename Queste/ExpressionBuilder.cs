ae2058b8db086ae3c923faf11bb43ebf8858a279
Samuel Butler <samuelbutler87@gmail.com>
2016-11-06 10:10:34 +1000

    ExpressionBuilder BuildExpression(string queryString) now returns p => true if no expressions were built using the passed in query string

--- Queste/ExpressionBuilder.cs
+++ Queste/ExpressionBuilder.cs
@@ -167,7 +167,7 @@ namespace Queste
 
       return expression != null
         ? Lambda<Func<TSource, bool>>(expression, parameter)
-        : null;
+        : p => true;
     }
 
     /// <summary>
